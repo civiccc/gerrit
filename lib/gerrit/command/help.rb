d7a18725d35de1c1df205d1e6a096a1847c7c891
Shane da Silva <shane@dasilva.io>
2015-08-04 22:34:58 -0700

    Display all available commands in help command
    Still leaves much to be desired, but this at least provides a reminder
    of the command name. Future work will hopefully add some basic
    descriptions as well.

--- lib/gerrit/command/help.rb
+++ lib/gerrit/command/help.rb
@@ -3,6 +3,20 @@ module Gerrit::Command
   class Help < Base
     def execute
       ui.print 'Usage: gerrit [command]'
+      ui.newline
+
+      ui.info 'Commands:'
+
+      commands.each do |command|
+        ui.print command
+      end
+    end
+
+    private
+
+    def commands
+      Dir[File.join(File.dirname(__FILE__), '*.rb')]
+        .map { |path| File.basename(path, '.rb') } - ['base']
     end
   end
 end
